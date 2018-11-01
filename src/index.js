82ed04cbfa0501c733304f9f29f18739615d7567
blissdismissed <ajautz@gmail.com>
2018-11-01 14:53:05 -0400

    add history capabilities

--- src/index.js
+++ src/index.js
@@ -42,6 +42,9 @@ function Square(props) {
     
     handleClick(i){
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -94,6 +97,18 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+      };
+    }
+
+
+
     render() {
       return (
         <div className="game">
